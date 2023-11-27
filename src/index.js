c963e3cad0fe7f7058906386744d73debc3a39e8
Anthony Huang <anthonyhuang07@icloud.com>
2023-11-27 12:53:21 -0500

    added countdown

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 const header = document.getElementById('header');
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    header.style.top = "0";
-  } else {
-    header.style.top = "-4.75rem";
-  }
-  prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-4.75rem";
+    }
+    prevScrollpos = currentScrollPos;
 }
+
+let countDownDate = new Date("December 15, 2023 15:37:25").getTime();
+
+function timer() {
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
+}
+
+timer()
+
+let x = setInterval(function () {
+
+    timer()
+
+    if (distance < 0) {
+        clearInterval(x);
+    }
+}, 1000);
