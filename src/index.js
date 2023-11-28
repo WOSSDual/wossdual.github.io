c9f240b146315704d5f1357f3647396d6767300c
Anthony Huang <anthonyhuang07@icloud.com>
2023-11-28 12:19:31 -0500

    header highlighting!

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-let prevScrollpos = window.pageYOffset;
+/* HEADER */
+
+let yPos = window.scrollY
 const header = document.getElementById('header');
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
+
+function headerHide(){
+    if (yPos < window.scrollY){
+        header.style.top = "-10vh";
+    } else if (yPos > window.scrollY){
         header.style.top = "0";
-    } else {
-        header.style.top = "-4.75rem";
     }
-    prevScrollpos = currentScrollPos;
+    yPos = window.scrollY
 }
+
+/* COUNTDOWN */
 
 let countDownDate = new Date("December 18, 2023 15:00:00").getTime();
 
 function timer() {
-    let now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    let distance = countDownDate - now;
+    let now = new Date().getTime();
+    let distance = countDownDate - now; 
 
     // Time calculations for days, hours, minutes and seconds
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -28,6 +32,10 @@ function timer() {
     document.getElementById("hours").innerHTML = hours
     document.getElementById("minutes").innerHTML = minutes
     document.getElementById("seconds").innerHTML = seconds
+
+    if (distance < 0) {
+        clearInterval(x);
+    }
 }
 
 timer()
@@ -36,7 +44,37 @@ let x = setInterval(function () {
 
     timer()
 
-    if (distance < 0) {
-        clearInterval(x);
-    }
 }, 1000);
+
+/* NAVBAR COLORING */
+
+function highlightCurrentNav() {
+
+    // Get the position of the sections
+    let homeSection = document.getElementById('home')
+    let aboutSection = document.getElementById('about')
+
+    // Select the navigation links
+    let homeNav = document.getElementById('nav-home');
+    let aboutNav = document.getElementById('nav-about');
+
+    // Reset styles for all navigation links
+    homeNav.style.color = 'white';
+    aboutNav.style.color = 'white';
+
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // Get the position of each section
+    const homePos = homeSection.offsetTop;
+    const aboutPos = aboutSection.offsetTop-1;
+
+    // Logic to highlight the current section's nav link
+    if (scrollPosition >= homePos && scrollPosition < aboutPos) {
+        homeNav.style.color = 'hsl(140, 60%, 45%)';
+    } else if (scrollPosition >= aboutPos) {
+        aboutNav.style.color = 'hsl(140, 60%, 45%)';
+    }
+};
+
+window.onscroll = highlightCurrentNav;
+window.onload = highlightCurrentNav;
